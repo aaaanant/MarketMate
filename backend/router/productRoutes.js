fedd8f9574ebf41d45b5ebb072e0ec10823fd663
aaaanant <anantbhatt789@gmail.com>
2026-04-25 23:53:18 +0530

    fix dummy api using axios

--- backend/router/productRoutes.js
+++ backend/router/productRoutes.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const Product = require("../model/product");
-const fetch = require("node-fetch");
+const axios = require("axios");
 
 router.post("/add", async (req, res) => {
   try {
@@ -23,12 +23,10 @@ router.post("/add", async (req, res) => {
 
 router.get("/dummy", async (req, res) => {
   try {
-    const response = await fetch("https://dummyjson.com/products?limit=12");
-    const data = await response.json();
-
-    res.json(data.products);
+    const response = await axios.get("https://dummyjson.com/products?limit=12");
+    res.json(response.data.products);
   } catch (err) {
-    console.log(err);
+    console.log(err.message);
     res.status(500).json({ message: "Error fetching dummy products" });
   }
 });
@@ -37,7 +35,6 @@ router.get("/all", async (req, res) => {
   try {
     const products = await Product.find();
     res.json(products);
-
   } catch (err) {
     res.status(500).json({ message: "Error fetching products" });
   }
