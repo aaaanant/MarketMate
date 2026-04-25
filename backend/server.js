eaa730a66e72400de35d7f0266283e62d069a9e2
aaaanant <anantbhatt789@gmail.com>
2026-04-25 19:27:14 +0530

    routes

--- backend/server.js
+++ backend/server.js
@@ -8,14 +8,13 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-// ✅ correct path (after fix)
 const authRoutes = require("./router/authRoutes");
 const cartRoutes = require("./router/cartRoutes");
 
 app.use("/api/auth", authRoutes);
 app.use("/api/cart", cartRoutes);
 
-mongoose.connect("mongodb://127.0.0.1:27017/marketmate")
+mongoose.connect(process.env.MONGO_URI)
   .then(() => console.log("DB Connected"))
   .catch((err) => console.log(err));
 
