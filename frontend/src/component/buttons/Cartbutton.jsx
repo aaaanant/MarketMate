0de7f8460333bb589f49dc1e03d33b39c581d527
aaaanant <anantbhatt789@gmail.com>
2026-04-29 14:52:56 +0530

    done

--- frontend/src/component/buttons/Cartbutton.jsx
+++ frontend/src/component/buttons/Cartbutton.jsx
@@ -2,28 +2,38 @@ import React from "react";
 import styles from "../../styles/button/cartbutton.module.css";
 
 function Cartbutton({ product, text = "Add to Cart" }) {
-  const handleAddToCart = () => {
-    let cart = JSON.parse(localStorage.getItem("cart")) || [];
+  const handleAddToCart = async () => {
+    try {
+      const cartId = localStorage.getItem("cartId");
 
-    const productId = String(product._id || product.id);
+      const res = await fetch(
+        `${import.meta.env.VITE_API_URL}/api/cart/add`,
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({
+            cartId,
+            product: {
+              id: product._id,
+              title: product.title,
+              price: product.price,
+              image: product.image,
+            },
+          }),
+        }
+      );
 
-    const existing = cart.find((item) => item.id === productId);
+      const data = await res.json();
+      console.log(data);
 
-    if (existing) {
-      existing.quantity += 1;
-    } else {
-      cart.push({
-        id: productId,
-        title: product.title,
-        price: product.price,
-        image: product.image,
-        quantity: 1,
-      });
-    }
-
-    localStorage.setItem("cart", JSON.stringify(cart));
-    window.dispatchEvent(new Event("cartUpdated"));
       alert("Added to cart");
+      window.dispatchEvent(new Event("cartUpdated"));
+
+    } catch (err) {
+      console.log(err);
+    }
   };
 
   return (
